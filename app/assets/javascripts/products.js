5b632fba8b4514246cd712c9d54d0da0d039c15c
yuyi1024 <bonnie831024@gmail.com>
2018-01-16 02:50:49 +0800

    test

---
+++ app/assets/javascripts/products.js
@@ -0,0 +1,37 @@
+$(document).on('turbolinks:load', function() {
+  
+  $('#selector .col-sm-3').click(function(){
+    $target = $($(this).data('target'));
+    $target.toggle();
+  });
+
+  $('#cat1 .col-sm-3').click(function(){
+    $target = $($(this).data('target'));
+    // if($target.is(':visible')){
+      
+    // }
+    //$target.toggle();
+
+
+
+    $('#cat1_field').val($(this).text());
+    $('#search_form').submit();
+  });
+
+ 
+
+
+
+
+  // $('.search .col-sm-3').click(function(){
+  //   $target = $($(this).data('target'))
+  //   $('.collection .col-sm-12').slideUp();
+  //   if(!$target.is(":visible")){
+  //     $target.slideDown();
+  //   }
+  // });
+    
+});
+
+
+
