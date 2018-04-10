8ca74ecc045254cf31e6fc3896329eb85fb4e95f
yuyi1024 <bonnie831024@gmail.com>
2018-04-10 18:18:08 +0800

    email sending

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  # ActionMailer Config
+  # Setup for production - deliveries, no errors raised
 end
