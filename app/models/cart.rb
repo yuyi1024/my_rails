8025064c51f1c4c9264c37a5fb518b00dd100432
yuyi1024 <bonnie831024@gmail.com>
2018-04-14 03:10:30 +0800

    用session儲存cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,65 @@
 class Cart < ApplicationRecord
 
+  #自動產生getter
+  attr_reader :items
+
+  # def items
+  #   return @items
+  # end
+
+  # def items=(new_item)
+  #   @items = new_item
+  # end
+
+  def initialize(items = [])
+    @items = item
+  end
+
+  def add_item(product_id, quantity)
+    found_item = @items.find{ |item| item.product_id == product_id } 
+
+    if found_item
+      found_item.increment(quantity)
+    else
+      @items << CartItem.new(product_id, quantity)
+    end
+  end
+
+  def total_price
+    @items.reduce(0){ |sum, item| sum + item.price }
+    #[].reduce(initial){ |memo, obj| block }
+  end
+
+  def to_hash
+    all_item = @items.map{ |item| {'product_id' => item.product_id, 'quantity' => item.quantity } }
+    { "items" => all_item }
+  end
+
+  # class method 類別方法
+  def self.from_hash(hash)
+    # @items = Cart.new
+    # if !hash.nil?
+    #   hash['items'].each do |item|
+    #     @items << CartItem.new( item['product_id'], item['quantity'] )
+    #   end
+    # end  
+
+    if hash.nil?
+      new []   #new([]) => initialize([])
+    else
+      new hash['items'].map{ |item|
+        CartItem.new(item['product_id'], item['quantity']) 
+      }
+    end  
+  end
+
+
+
+  # def empty?
+  #   @items.empty?
+  # end
+
+  
+
 
 end
---
+++ app/models/cart_item.rb
@@ -0,0 +1,22 @@
+class CartItem < ApplicationRecord
+
+  attr_reader :product_id, :quantity
+
+  def initialize(product_id, quantity = 1)
+    @product_id = product_id
+    @quantity = quantity
+  end
+
+  def increment(quantity = 1)
+    @quantity += quantity
+  end
+
+  def product
+    Product.find(@product_id)
+  end
+
+  def price
+    product.price * @quantity
+  end
+
+end
