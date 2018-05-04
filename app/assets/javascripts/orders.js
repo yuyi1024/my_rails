3d444123eddc22d965c484b56ed24793d150d03a
yuyi1024 <bonnie831024@gmail.com>
2018-05-05 01:46:14 +0800

    order

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -1,11 +1,5 @@
 $(document).on('turbolinks:load', function() {
 
-	//若 EZship 有回傳值，即選擇超商取貨 radio
-	if($('#store_value').text() != ''){
-		document.getElementById("in_store").checked = true;
-		$('#ship_form').submit();
-	}
-
 });
 
 function selectShip(){ //選擇送貨方式
@@ -14,18 +8,6 @@ function selectShip(){ //選擇送貨方式
 		$('#user_data').val($user_data_chk.checked);
 	}
 	$('#ship_form').submit();
-}
-
-
-function form_required(){
-	var required = true;
-	$('#new_order .row input').each(function(){
-		if($(this).val() == ''){
-			required = false;
-			alert($(this).prev().text()+'尚未填寫');
-		}
-	});
-	return required;
 }
 
 //new_order送貨方式選擇
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,6 +3,7 @@
   protect_from_forgery with: :null_session, only: [:ezship]
 
   def new #購物車頁面
+    #將 cart session 中的資料存入新產生的 order session，以防結帳後再更動 cart
     @cart_session = Cart.from_hash(session[Cart::SessionKey_cart])
     @order_session = Cart.new_order_hash(@cart_session)
     session[Cart::SessionKey_order] = @order_session.to_hash
@@ -40,41 +41,34 @@
     @order_session.items.length.times{@order.order_items.build}
     @order_session.session_to_order_items(@order)
 
+    @cart_session = session[Cart::SessionKey_cart]
+    @order_session = @order_session.to_hash
 
     if @order.save
+
+      # 刪除 cart session 中已結帳的物品，未結帳的不動
+      @order_session['items'].each do |item|
+        @cart_session['items'] = @cart_session['items'].delete_if{|key,_| key['product_id'] == item['product_id'].to_s}
+      end
+
+      session[Cart::SessionKey_cart] = @cart_session
+      session[Cart::SessionKey_order] = Cart.new
+
       redirect_to edit_order_path(@order)
     else
-      # redirect_to new_order_path
+      redirect_to new_order_path
     end
   end
 
   def edit #訂單資料填寫
-    # 按下結帳後將 cart session 中的資料存入新產生的 order session，以防結帳後再更動 cart
-    # @cart_session = Cart.from_hash(session[Cart::SessionKey_cart])
-    # @order_session = Cart.new_order_hash(@cart_session)
-    # session[Cart::SessionKey_order] = @order_session.to_hash
     @order = Order.find(params[:id])
 
     if !params[:stName].nil?
       @stName = params[:stName]
     end
 
-
     @client_token = Braintree::ClientToken.generate
   end
-
-  # def ship_method #選擇寄送方式並改變 total_price、收件資料 form
-  #   @order = Order.new
-  #   @total_price_with_ship = Cart.from_hash(session[Cart::SessionKey_cart]).total_price
-  #   @ship_method = params[:ship_method] 
-  #   @user = current_user if params[:user_data] == 'true'
-
-  #   if @ship_method == 'in_store'
-  #     @total_price_with_ship += Order::Freight_in_store
-  #   elsif @ship_method == 'to_address'
-  #     @total_price_with_ship += Order::Freight_to_address
-  #   end
-  # end
 
   def to_ezship #傳至 EZship
     @order = Order.find_by(process_id: params[:process_id])
@@ -96,55 +90,17 @@
     @order = Order.find(params[:id])
     @order.update(order_params)
     if @order.save
-      redirect_to products_path
+      flash[:notice] = '訂單建立'
+      if @order.pay_method == 'atm'
+        redirect_to remit_info_orders_path(@order.process_id)
+      else
+        redirect_to products_path
+      end
+    else
+      redirect_to edit_order_path(@order)
     end
+
   end
-
-  # def create
-  #   @cart_session = Cart.from_hash(session[Cart::SessionKey_cart])
-  #   @order_session = Cart.from_hash(session[Cart::SessionKey_order])
-
-  #   @order = Order.create(order_params)
-  #   @order.user = current_user
-  #   @order.price = @order_session.total_price #不含運
-  #   @order.process_id = @order.g_process_id(current_user.id, current_user.orders.length+1)
-
-  #   if order_params[:ship_method] == 'in_store'
-  #     @order.freight = Order::Freight_in_store
-  #   elsif order_params[:ship_method] == 'to_address'
-  #     @order.freight = Order::Freight_to_address
-  #   end
-
-  #   # 將 order session 的東西存入 new 的 OrderItem 中
-  #   @order_session.items.length.times{@order.order_items.build}
-  #   @order_session.session_to_order_items(@order) 
-
-  #   @cart_session = @cart_session.to_hash
-  #   @order_session = @order_session.to_hash
-
-  #   if @order.save
-
-  #     # 刪除 cart session 中已結帳的物品，未結帳的不動
-  #     @order_session['items'].each do |item|
-  #       @cart_session['items'] = @cart_session['items'].delete_if{|key,_| key['product_id'] == item['product_id'].to_s}
-  #     end
-
-  #     session[Cart::SessionKey_cart] = @cart_session
-  #     session[Cart::SessionKey_order] = Cart.new
-
-  #     flash[:notice] = '訂單建立'
-
-  #     if order_params[:pay_method] == 'pay_before'
-  #       redirect_to remit_info_orders_path(@order.process_id)
-  #     else
-  #       redirect_to products_path
-  #     end
-      
-  #   else
-  #     redirect_to new_order_path
-  #   end
-
-  # end
 
   def remit_info
     @order = current_user.orders.find_by(process_id: params[:process_id])
