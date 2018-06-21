7b1c384683698e92e77a61cae1bf4fe9c06c082e
yuyi1024 <bonnie831024@gmail.com>
2018-06-22 00:49:31 +0800

    修

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,6 +11,8 @@ class ProductsController < ApplicationController
     if params[:cat1_field].present?
       cat1 = Category.find_by(name: params[:cat1_field])
       @cat2s = cat1.subcategories.joins(:product).group('subcategory_id').having('count(subcategory_id) > 0')
+      puts @cat2s
+      puts 'xxxxxxxxxx'
       @products = cat1.product.where(status: 'on_shelf')
     end
 
