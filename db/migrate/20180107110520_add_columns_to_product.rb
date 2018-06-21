14a8aa8b8bb50df2c283fe26757eaac153deffc7
yuyi1024 <bonnie831024@gmail.com>
2018-06-21 18:46:47 +0800

    修

--- db/migrate/20180107110520_add_columns_to_product.rb
+++ db/migrate/20180107110520_add_columns_to_product.rb
@@ -1,6 +1,5 @@
 class AddColumnsToProduct < ActiveRecord::Migration[5.1]
   def change
-    remove_column :products, :catrgory
     add_column :products, :category_id, :integer
     change_column :categories, :cat1, :integer
   end
