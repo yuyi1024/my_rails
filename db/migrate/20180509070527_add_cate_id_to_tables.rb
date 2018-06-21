a2c502d935772abba98f5f24b987411faac5e7c4
yuyi1024 <bonnie831024@gmail.com>
2018-06-21 19:05:28 +0800

    修

--- db/migrate/20180509070527_add_cate_id_to_tables.rb
+++ db/migrate/20180509070527_add_cate_id_to_tables.rb
@@ -1,6 +1,5 @@
 class AddCateIdToTables < ActiveRecord::Migration[5.1]
   def change
-    change_column :subcategories, :category_id, :integer, after: :name
     add_column :products, :subcategory_id, :integer, after: :category_id
   end
 end
