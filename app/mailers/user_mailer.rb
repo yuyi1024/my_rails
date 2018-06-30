11b81ad79a5bcd8f30f6547edaf721a51b69a13b
yuyi1024 <bonnie831024@gmail.com>
2018-06-30 20:57:45 +0800

    ActionMailer Config

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ApplicationMailer
   def message_email(message)
     @user = message.user
     @message = message
-    @url = 'http://localhost:3001/messages'
+    @url = 'https://bawan-store-0225.herokuapp.com/messages'
     mail( to: @user.email, subject: '霸雕爆裂丸-訊息回覆' )
   end
 end
